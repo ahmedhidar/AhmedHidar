3d6898aaf7b84ff79c7f62afc7de66ab28ac9316
ahmed hidar <ahmedheedar@gmail.com>
2025-08-07 16:15:16 +0300

    fix new errors for deployment

--- app/page.tsx
+++ app/page.tsx
@@ -1,31 +1,11 @@
-"use client";
+// app/page.tsx أو pages/index.tsx
 
-import { navItems } from "@/data";
+import dynamic from "next/dynamic";
 
-import Hero from "@/components/Hero";
-import Grid from "@/components/Grid";
-import Footer from "@/components/Footer";
-import Clients from "@/components/Clients";
-import Approach from "@/components/Approach";
-import Experience from "@/components/Experience";
-import RecentProjects from "@/components/RecentProjects";
-import { FloatingNav } from "@/components/ui/FloatingNavbar";
+const HomePage = dynamic(() => import("@/components/HomePage"), {
+  ssr: false,
+});
 
-const Home = () => {
-  return (
-    <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
-      <div className="max-w-7xl w-full">
-        <FloatingNav navItems={navItems} />
-        <Hero />
-        <Grid />
-        <RecentProjects />
-        {/* <Clients /> */}
-        <Experience />
-        <Approach />
-        <Footer />
-      </div>
-    </main>
-  );
-};
-
-export default Home;
+export default function Page() {
+  return <HomePage />;
+}
