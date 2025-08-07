dad99d39d77544e27d31a06ea3bcab55be2b7e3a
ahmed hidar <ahmedheedar@gmail.com>
2025-08-07 16:09:11 +0300

    fix new errors for deployment

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -9,10 +9,12 @@ const Footer = () => {
     <footer className="w-full pt-20 pb-10" id="contact">
       {/* background grid */}
       <div className="w-full absolute left-0 -bottom-72 min-h-96">
-        <img
+        <Image
           src="/footer-grid.svg"
           alt="grid"
           className="w-full h-full opacity-50 "
+          fill
+          priority
         />
       </div>
 
